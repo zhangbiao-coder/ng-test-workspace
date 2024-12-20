9e244400dbe3abc62a2f77991c02f1118d17bf04
张彪 <845772828@qq.com>
2024-12-20 15:42:46 +0800

    测试html页面导出excel

--- projects/test-html-export-excel/src/app/app.component.ts
+++ projects/test-html-export-excel/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component, ElementRef, ViewChild} from '@angular/core';
-import {ExcelUtils} from './util/ExcelUtils';
+import {ToExcelOption, ZETUtil} from 'z-export-table';
 
 @Component({
   selector: 'app-root',
@@ -8,14 +8,15 @@ import {ExcelUtils} from './util/ExcelUtils';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  @ViewChild("table") table:ElementRef|undefined;
+  @ViewChild("table") table: ElementRef | undefined;
 
   exportsExcel() {
-    if(this.table){
-      ExcelUtils.exportTableToExcel(this.table.nativeElement.outerHTML,{
-        filename:"复杂html导出Excel测试",
-        userCss:`table { width: 100%; border-collapse: collapse; margin: 20px 0; font-size: 14px; text-align: center; } th, td { border: 1px solid #ddd; padding: 10px; } .highlight { background-color: #ffeb3b; font-weight: bold; } .merged { background-color: #ffc107; } .header { background-color: #ff7043; color: white; font-weight: bold; }`
-      })
+    let option: ToExcelOption = {
+      filename: "复杂html导出Excel测试",
+      userCss: `table { width: 100%; border-collapse: collapse; margin: 20px 0; font-size: 14px; text-align: center; } th, td { border: 1px solid #ddd; padding: 10px; } .highlight { background-color: #ffeb3b; font-weight: bold; } .merged { background-color: #ffc107; } .header { background-color: #ff7043; color: white; font-weight: bold; }`
+    };
+    if (this.table) {
+      ZETUtil.exportTableToExcel(this.table.nativeElement.outerHTML, option);
     }
   }
 }
